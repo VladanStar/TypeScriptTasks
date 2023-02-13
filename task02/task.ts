c609ebf043a5946c555d0d9790a50de1fd3f094b
VladanStar <vladancupric@gmail.com>
2023-02-13 10:45:59 +0100

    done

--- task02/task.ts
+++ task02/task.ts
@@ -22,4 +22,20 @@ class Osoba {
   console.log(osoba1.ime + " " + osoba1.prezime + " ima " + osoba1.izračunajStarost() + " godina.");
   console.log(osoba2.ime + " " + osoba2.prezime + " ima " + osoba2.izračunajStarost() + " godina.");
   console.log(osoba3.ime + " " + osoba3.prezime + " ima " + osoba3.izračunajStarost() + " godina.");
+
+  const osobe = [
+    new Osoba("John", "Doe", 1990),
+    new Osoba("Jane", "Doe", 1995),
+    new Osoba("Jim", "Smith", 1985)
+  ];
   
+  const filtriraneOsobe = osobe.filter(osoba => osoba. izračunajStarost() >= 30);
+  
+  console.log("Filtrirane osobe: ");
+  for (const osoba of filtriraneOsobe) {
+    console.log("Ime: " + osoba.ime);
+    console.log("Prezime: " + osoba.prezime);
+    console.log("Godine: " + osoba.godine);
+    console.log("Starost: " + osoba.izračunajStarost() + " godina");
+    console.log("");
+  }
