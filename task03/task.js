c2ff89f8d064998ddea80e515cad314452fbb535
VladanStar <vladancupric@gmail.com>
2023-02-13 10:05:17 +0100

    Add

---
+++ task03/task.js
@@ -0,0 +1,9 @@
+"use strict";
+const brojevi = [10, 5, 30, 20];
+console.log("Originalni niz: " + brojevi);
+// Sortiranje niza
+brojevi.sort((a, b) => a - b);
+console.log("Sortirani niz: " + brojevi);
+// Obrnuti ispis elemenata niza
+brojevi.reverse();
+console.log("Obrnuti niz: " + brojevi);
